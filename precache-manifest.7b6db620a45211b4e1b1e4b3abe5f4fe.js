f12004a44777083ab5afffaea00413dd118c7540
sanz <berest915@gmail.com>
2020-02-01 11:17:21 +0800

    Updates

---
+++ precache-manifest.7b6db620a45211b4e1b1e4b3abe5f4fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe264f811df148462788ad981d8d0b0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24288b878afd78347c6a",
+    "url": "/static/css/main.367fe381.chunk.css"
+  },
+  {
+    "revision": "669c37eeb262f3f45640",
+    "url": "/static/js/2.9ddf858d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.9ddf858d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24288b878afd78347c6a",
+    "url": "/static/js/main.ec5bb7f4.chunk.js"
+  },
+  {
+    "revision": "e7be2421d4d75481ec56",
+    "url": "/static/js/runtime-main.2a529da5.js"
+  }
+]);
