c94d649789a51a512f9abe3a0d19a7da7137a77f
elude21933 <43527518+rest-media@users.noreply.github.com>
2020-02-17 21:57:51 +0800

    Rolling back to stable version

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,15 +5,15 @@ import Spinner from '../layout/Spinner'
 import AboutUserItemInOne from './AboutUserItemInOne'
 
 const About = () => {
-  const { users, loop, resetLoop, clone_users, fetchUsers, loading, cloneUsers } = useContext(GithubContext)
+  const { loading } = useContext(GithubContext)
   
  
-  useEffect(() => {
-    console.log('useEffect called')
-    fetchUsers()
+  // useEffect(() => {
+  //   console.log('useEffect called')
+  //   fetchUsers()
    
-    // eslint-disable-next-line
-  }, []) 
+  //   // eslint-disable-next-line
+  // }, []) 
  
   if (loading) return <Spinner />
   
@@ -23,7 +23,7 @@ const About = () => {
       <h1>About this App</h1>
       <p>hey this is to search for github users</p>
       <p>version 1.0.0</p>
-      <br />
+      {/* <br />
       <br/>
       <h1>Failed Attempt, planned to exclude it before building>deploying</h1>
       <h2>A sample to test the response from github-api </h2>
@@ -33,10 +33,8 @@ const About = () => {
         <AboutUserItem key={user.id} user={user} 
           clone_users={clone_users}
         />
-      ))}
+      ))} */}
 
-    
- 
     </Fragment>
   )
 }
